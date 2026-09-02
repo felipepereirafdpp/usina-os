dd5fa2629d2f88ba8752dea0f67e2498f5059c9c
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-09-02 15:58:56 -0300

    fix : arruma conflitos

--- Interfaces/Cliente/IClienteService.cs
+++ Interfaces/Cliente/IClienteService.cs
@@ -9,19 +9,10 @@ namespace UsinaOS.Interfaces.Cliente
     public interface IClienteService
     {
         Task<ClienteResponse> CadastrarCliente(CreateCliente InformacoesCliente);
-<<<<<<< HEAD
-        Task<ClienteResponse> BuscarClienteID(Guid IdCliente);
-        Task<ClienteResponse> BuscarClienteCnpj(string CnpjCliente);
-        Task<List<ClienteResponse>> ListarClientes();
-        Task<ClienteResponse> AtualizarCliente(UpdateCliente InformacoesCliente);
-        Task<bool> DeletarCliente(Guid IdCliente);
-
-=======
         Task<ClienteResponse> BuscarClientePorIdPorCnpj(Guid? IdCliente, string? cnpjCliente);
         Task<List<ClienteResponse>> ListarClientes();
         Task<ClienteResponse> AtualizarCliente(string cnpjCliente, UpdateCliente InformacoesCliente);
         Task<bool> DeletarCliente(string cnpjCliente);
->>>>>>> feature/criando-service
  
     }
 }
